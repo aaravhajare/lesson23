5e280ef96821d54936756a4639ac7bd4cc552fd3
aaravhajare <hajareaarav66@gmail.com>
2025-04-14 10:05:04 +0530

    That’s the exit

---
+++ That’s the exit.py
@@ -0,0 +1,14 @@
+
+for i in range(10):
+    
+    # if the value becomes 5 the program is forced to exit
+
+    if i == 5:
+
+        #print exit message 
+        print(exit)
+
+        exit()
+
+    print(i)
+        
